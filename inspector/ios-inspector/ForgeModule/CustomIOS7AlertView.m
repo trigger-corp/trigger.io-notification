abb84e1bff5076c95b9f0dab8016d2c707625225
Antoine van Gelder <antoine@trigger.io>
2014-09-03 12:26:36 +0200

    Support arm64 devices running iOS 8

--- inspector/ios-inspector/ForgeModule/CustomIOS7AlertView.m
+++ inspector/ios-inspector/ForgeModule/CustomIOS7AlertView.m
@@ -203,14 +203,14 @@ CGFloat buttonSpacerHeight = 0;
     }
     
     if (onButtonTouchUpInside != NULL) {
-        onButtonTouchUpInside(self, [sender tag]);
+        onButtonTouchUpInside(self, (int)[sender tag]);
     }
 }
 
 // Default button behaviour
 - (void)customIOS7dialogButtonTouchUpInside: (CustomIOS7AlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    NSLog(@"Button Clicked! %d, %d", buttonIndex, [alertView tag]);
+    NSLog(@"Button Clicked! %ld, %ld", (long)buttonIndex, (long)[alertView tag]);
     [self close];
 }
 
